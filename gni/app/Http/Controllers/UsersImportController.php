941fd7d867fe0bc342c2a8e4c586cdc13f16b7d8
littleOkittenOwantsOsugar <topevol33@gmail.com>
2022-06-03 20:57:37 +0300

    creating

--- gni/app/Http/Controllers/UsersImportController.php
+++ gni/app/Http/Controllers/UsersImportController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class UsersImportController extends Controller
 {
-    //
+    public function show(){
+        return view('users.import');
+    }
+    public function store(){
+
+    }
 }
--- gni/routes/web.php
+++ gni/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/import', 'UsersImportController@show');
+Route::post('/users/import', 'UsersImportController@store');
