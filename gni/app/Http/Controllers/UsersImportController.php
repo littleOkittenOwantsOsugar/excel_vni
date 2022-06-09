ab186a1caa1492593b649c2f48cb0fc8af081858
littleOkittenOwantsOsugar <topevol33@gmail.com>
2022-06-09 16:50:57 +0300

    import

--- gni/app/Http/Controllers/UsersImportController.php
+++ gni/app/Http/Controllers/UsersImportController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Imports\UsersImport;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel
 
 class UsersImportController extends Controller
 {
     public function show(){
         return view('users.import');
     }
-    public function store(){
+    public function store(Request $request)
+    {
+        $file=$request->file('file');
 
+        Excel::import(new UsersImport, $file);
+
+        return back()->withStatus('Excel file imported succesfully');
     }
 }
--- gni/app/Imports/UsersImport.php
+++ gni/app/Imports/UsersImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class UsersImport implements ToModel
@@ -15,7 +16,9 @@ class UsersImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            //
+            'name' => $row[0],
+            'email' => $row[1],
+            'password' => Hash::make('password')
         ]);
     }
 }
