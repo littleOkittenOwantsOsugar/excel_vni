b671c3255308ff9db5ef8d499cd072b149e70164
littleOkittenOwantsOsugar <topevol33@gmail.com>
2022-07-05 13:41:59 +0300

    made templates for importing 1-5 sheets from excel, deleted unnecessary table, import 70% completed

--- gni/app/Imports/SecondSheetImport.php
+++ gni/app/Imports/SecondSheetImport.php
@@ -2,16 +2,43 @@
 
 namespace App\Imports;
 
+use App\Models\LicenseArea;
+use App\Models\StatusOfLicense;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class SecondSheetImport implements ToCollection
+class SecondSheetImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param Collection $collection
     */
-    public function collection(Collection $collection)
+    
+
+    public function collection(Collection $rows)
     {
-        //
+        
+        foreach ($rows as $row) 
+            {
+                $LAtemp=explode(" (", $row[3] );//Таволожская (Блок А) (СРТ01113НР)
+                //Таволожская, Блок А), СРТ01113НР)
+        
+                StatusOfLicense::firstorcreate([
+                    'NameStatus' => $row[0]
+                ]);
+                
+                if (array_key_exists(2, $LAtemp)){
+                    $LAtemp=(string)$LAtemp;
+                    $LAtemp=substr($LAtemp, 0, -1);
+                    $LAtemp=(object)$LAtemp;
+                    $LAtemp->implode(" (", );
+                };
+                    //$LAtemp->implode(" (", )
+                
+
+                LicenseArea::create([
+                    'NameLicenseArea' //the last() 
+                ]);
+            }
     }
 }
--- gni/app/Imports/ThirdSheetImport.php
+++ gni/app/Imports/ThirdSheetImport.php
@@ -2,16 +2,55 @@
 
 namespace App\Imports;
 
+use App\Models\Agency;
+use App\Models\Company;
+use App\Models\License;
+use App\Models\LicenseArea;
+use App\Models\SpecialPurpose;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ThirdSheetImport implements ToCollection
+class ThirdSheetImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param Collection $collection
     */
-    public function collection(Collection $collection)
+    private $company_temp;
+
+    public function __construct()
     {
-        //
+        $this -> company_temp = Company::select('id_company', 'NameCompany')->get();
+    }
+
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) 
+            {
+
+                License::create([
+                    'id_company',
+                    'id_license_area',
+                    'id_agency',
+                    'id_status',
+                    'PreviousLicense',//links to another license
+                    'TypeOfPrimaryMineral' => $row[7],
+                    'DateOfRegistration' => $row[10],
+                    'DateOfEnding' => $row[11],
+                    'DateOfСancellation' => $row[12],
+                    'Seria' =>$row[2],
+                    'Number' => $row[3],
+                    'Type' => $row[4],
+                    'SpecialPurpose' => $row[6]
+                ]);
+                if ($row[13] != null){
+                    Agency::firstorcreate([
+                        'NameAgency' => $row[13]
+                    ]);
+                }
+                LicenseArea::create([
+                    'Geometry'
+                ]);
+            }
     }
 }
