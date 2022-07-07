6e6988f532cef9452938e4c0cdd712ff429b1ee7
littleOkittenOwantsOsugar <topevol33@gmail.com>
2022-07-07 13:25:52 +0300

    fixed a little bit, processing import

--- gni/app/Imports/ThirdSheetImport.php
+++ gni/app/Imports/ThirdSheetImport.php
@@ -16,24 +16,18 @@ class ThirdSheetImport implements ToCollection, WithHeadingRow
     /**
     * @param Collection $collection
     */
-    private $company_temp;
-
-    public function __construct()
-    {
-        $this -> company_temp = Company::select('id_company', 'NameCompany')->get();
-    }
 
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) 
             {
+                //first without PreviousLicense then lookup for license in another foreach update
 
                 License::create([
                     'id_company',
                     'id_license_area',
                     'id_agency',
                     'id_status',
-                    'PreviousLicense',//links to another license
                     'TypeOfPrimaryMineral' => $row[7],
                     'DateOfRegistration' => $row[10],
                     'DateOfEnding' => $row[11],
@@ -49,8 +43,36 @@ class ThirdSheetImport implements ToCollection, WithHeadingRow
                     ]);
                 }
                 LicenseArea::create([
-                    'Geometry'
+                    'Geometry' => $row[14]
                 ]);
             }
+        foreach ($rows as $row) {
+            //СЫК01069НП
+            $tmp_row=array($row[5]);//3+5+2
+            //(С, Ы, К, 0, 1, 0, 6, 9, Н, П)
+            $tmp_row1=array($tmp_row[0], $tmp_row[1], $tmp_row[2]);
+            //(С, Ы, К)
+            $tmp_row1=implode("", $tmp_row1);
+            //СЫК
+            $tmp_row2=array($tmp_row[3], $tmp_row[4], $tmp_row[5], $tmp_row[6], $tmp_row[7]);
+            //(0, 1, 0, 6, 9)
+            $tmp_row2=implode("", $tmp_row2);
+            //01069
+            $tmp_row3=array($tmp_row[8], $tmp_row[9]);
+            //(Н, П)
+            $tmp_row3=implode("", $tmp_row3);
+            //НП
+            if ($row[2]=$tmp_row1 && $row[3]=$tmp_row2 && $row[4]=$tmp_row3){
+                //update for specific row
+                License::updateOrCreate(['PreviousLicense'=>$row[5]]);
+                //=id_license
+                //$user = User::where('email', request('email'))->first();
+                // if ($user !== null) {
+
+                //     $user->update(['name' => request('name')]);
+                
+                // }
+            }
+        }
     }
 }
